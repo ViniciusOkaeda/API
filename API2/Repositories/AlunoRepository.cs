0736e3a5ce829d5c1b03b60f2260bd9bf5542242
vinicius <viniciusokaeda@gmail.com>
2020-09-01 17:37:19 -0300

    aula 2 finalizada

---
+++ API2/Repositories/AlunoRepository.cs
@@ -0,0 +1,37 @@
+﻿using API2.Domains;
+using API2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API2.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+        Aluno IAluno.Alterar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        Aluno IAluno.BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Aluno IAluno.Cadastrar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        Aluno IAluno.Excluir(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Aluno> IAluno.LerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
